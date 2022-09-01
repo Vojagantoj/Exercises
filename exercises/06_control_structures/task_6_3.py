4574e39b219f8ea70aa86ceb8fb41f2d5b453105
Vojagantoj <42981253+Vojagantoj@users.noreply.github.com>
2022-09-01 14:18:56 +0300

    Update task_6_3.py
    "Last exersice"

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -52,20 +52,11 @@ interface FastEthernet0/7
 а access закомментировать.
 """
 
-access_template = [
-    "switchport mode access",
-    "switchport access vlan",
-    "spanning-tree portfast",
-    "spanning-tree bpduguard enable",
-]
-
 trunk_template = [
     "switchport trunk encapsulation dot1q",
     "switchport mode trunk",
     "switchport trunk allowed vlan",
 ]
-
-access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
 trunk = {
     "0/1": ["add", "10", "20"],
     "0/2": ["only", "11", "30"],
@@ -74,10 +65,18 @@ trunk = {
     "0/7": ["only", "30"],
 }
 
-# for intf, vlan in access.items():
-#     print("interface FastEthernet" + intf)
-#     for command in access_template:
-#         if command.endswith("access vlan"):
-#             print(f" {command} {vlan}")
-#         else:
-#             print(f" {command}")
+for intf, vlan in trunk.items():
+    if vlan[0] == "add":
+        vlan.remove('add')
+        vlans = ('add' + ' ' + ','.join(vlan))
+    elif vlan[0] == 'only':
+        vlan.remove('only')
+        vlans = (','.join(vlan))
+    elif vlan[0] == 'del':
+        vlan.remove('del')
+        vlans = ('remove' + ' ' + ','.join(vlan))
+    for command in trunk_template:
+        if command.endswith("allowed vlan"):
+            print(f" {command} {vlans}")
+        else:
+            print(f" {command}")
