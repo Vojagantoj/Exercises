28cabf0970630b60eca5945d642b340b1965a90c
Vojagantoj <kyznecev27@gmail.com>
2022-09-21 09:40:57 +0000

    3-4 exercises

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -47,3 +47,34 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    """
+    - intf_vlan_mapping: ожидает как аргумент словарь с соответствием интерфейс-VLANы
+      такого вида:
+        {'FastEthernet0/1': [10, 20],
+         'FastEthernet0/2': [11, 30],
+         'FastEthernet0/4': [17]}
+    - trunk_template: ожидает как аргумент шаблон конфигурации trunk-портов в виде
+      списка команд (список trunk_mode_template)
+
+    Возвращает список всех портов в режиме trunk с конфигурацией на основе шаблона
+    """
+    h = {}
+    for data in intf_vlan_mapping:
+        y = intf_vlan_mapping[data]
+        for n in trunk_template:
+            if n.endswith('vlan'):
+                k = trunk_template.index(n)
+                y = str(y).strip('[]').replace(' ','')
+                t = n + ' ' + y
+                l = trunk_template.copy()
+                l.remove(n)
+                l.insert(k , t)
+                m = dict([(data,l)])
+                h.update(m)
+                n = str()
+                break
+        continue
+    return h
+print(generate_trunk_config(trunk_config, trunk_mode_template))
